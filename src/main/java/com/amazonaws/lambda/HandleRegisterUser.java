beb7037acce860912ab298840cc4b23eaaa6d290
Isabel Morales Sirgo <imoralessirgo@outlook.com>
2021-11-03 17:04:16 -0400

    fix maven import

---
+++ src/main/java/com/amazonaws/lambda/HandleRegisterUser.java
@@ -0,0 +1,15 @@
+package com.amazonaws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+
+public class HandleRegisterUser implements RequestHandler {
+
+	@Override
+	public Object handleRequest(Object input, Context context) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
