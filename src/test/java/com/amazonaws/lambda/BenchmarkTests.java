833d78ff775561f97dc25d23dcb971f66b17119e
jcybul <jcybul@wpi.edu>
2021-12-14 18:04:51 -0500

    finished test for responses

--- src/test/java/com/amazonaws/lambda/BenchmarkTests.java
+++ src/test/java/com/amazonaws/lambda/BenchmarkTests.java
@@ -25,6 +25,21 @@ import com.amazonaws.entities.Implementation;
 import com.amazonaws.entities.MachineConfiguration;
 import com.amazonaws.entities.ProblemInstance;
 import com.amazonaws.entities.User;
+import com.amazonaws.http.CreateBenchmarkResponse;
+import com.amazonaws.http.CreateClassificationResponse;
+import com.amazonaws.http.CreateProblemInstanceResponse;
+import com.amazonaws.http.GetBenchmarkResponse;
+import com.amazonaws.http.GetImplementationsAllResponse;
+import com.amazonaws.http.GetProblemInstancesAllResponse;
+import com.amazonaws.http.MergeClassificationResponse;
+import com.amazonaws.http.ReclassifyAlgorithmResponse;
+import com.amazonaws.http.RegisterUserResponse;
+import com.amazonaws.http.RemoveAlgorithmResponse;
+import com.amazonaws.http.RemoveBenchmarkResponse;
+import com.amazonaws.http.RemoveClassificationResponse;
+import com.amazonaws.http.RemoveImplementationResponse;
+import com.amazonaws.http.RemoveProblemInstanceResponse;
+import com.amazonaws.http.RemoveUserResponse;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.util.json.Jackson;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -73,8 +88,60 @@ public class BenchmarkTests {
 
 			String incoming =  "{\"problem_instance\": \""+piID+"\",\"implementation\":\""+Impid+"\",\"user\": \"test1\",\"runtime\": 3,\"name\":\"test1\",\"cores\":3,\"L3\":\"l3\",\"L2\":\"l2\",\"L1\":\"l1\",\"threads\":2,\"cpu\":\"i7\",\"observation\":\"obs\"}";
 			String outgoing = "";
-
-
+			CreateClassificationResponse r = new CreateClassificationResponse(200,"");
+			CreateClassificationResponse r2 = new CreateClassificationResponse("",500);
+			r.toString();r2.toString();
+			
+			RemoveBenchmarkResponse rr = new RemoveBenchmarkResponse(200,"");
+			RemoveBenchmarkResponse rr2 = new RemoveBenchmarkResponse(500,"");
+			rr.toString();rr2.toString();
+			
+			RemoveProblemInstanceResponse rir = new RemoveProblemInstanceResponse(200,"");
+			RemoveProblemInstanceResponse rir2 = new RemoveProblemInstanceResponse(500,"");
+			rir.toString();rir2.toString();
+			
+			RemoveClassificationResponse rcr = new RemoveClassificationResponse(200,"");
+			RemoveClassificationResponse rcr2 = new RemoveClassificationResponse(500,"");
+			rcr.toString(); rcr2.toString();
+			
+			RemoveAlgorithmResponse rar = new RemoveAlgorithmResponse(200,"");
+			RemoveAlgorithmResponse rar2 = new RemoveAlgorithmResponse(500,"");
+			rar.toString();rar2.toString();
+			
+			ReclassifyAlgorithmResponse rear = new  ReclassifyAlgorithmResponse(200,"");
+			ReclassifyAlgorithmResponse rear2 = new  ReclassifyAlgorithmResponse(500,"");
+			rear.toString();rear2.toString();
+			
+			MergeClassificationResponse MCR = new MergeClassificationResponse(200,"");
+			MergeClassificationResponse MCR2 = new MergeClassificationResponse(500,"");
+			MCR.toString();MCR2.toString();
+			
+			RemoveUserResponse RUR = new RemoveUserResponse();
+			RemoveUserResponse RUR2 = new RemoveUserResponse("",500,"");
+			RUR2.toString();
+			
+			CreateProblemInstanceResponse CPIR = new CreateProblemInstanceResponse("",200,"");
+			CreateProblemInstanceResponse CPIR2 = new CreateProblemInstanceResponse("",500,"");
+			CPIR.toString();CPIR2.toString();
+			
+			RemoveImplementationResponse RIM = new RemoveImplementationResponse(200,"");
+			RemoveImplementationResponse RIM2 = new RemoveImplementationResponse(500,"");
+			RIM.toString();RIM2.toString();
+			
+			RegisterUserResponse RURr = new RegisterUserResponse(null,200,"");
+			RegisterUserResponse RURr2 = new RegisterUserResponse(null,500,"");
+			RURr.toString();RURr2.toString();
+			
+			GetProblemInstancesAllResponse GPIA = new GetProblemInstancesAllResponse(200,"");
+			GetProblemInstancesAllResponse GPIA2 = new GetProblemInstancesAllResponse(500,"");
+			GPIA.toString();GPIA.toString();
+			
+			GetImplementationsAllResponse GIAR = new GetImplementationsAllResponse(200,"");
+			GetImplementationsAllResponse GIAR2 = new GetImplementationsAllResponse(500,"");
+			GIAR.toString();GIAR2.toString();
+			
+			
+			
 
 			try {
 				CreateBenchmarkHandler handler  = new CreateBenchmarkHandler();
@@ -115,6 +182,13 @@ public class BenchmarkTests {
 		MachineConfigurationDAO mb = new MachineConfigurationDAO();
 		ImplementationDAO imdb = new ImplementationDAO();
 		
+		CreateBenchmarkResponse res = new CreateBenchmarkResponse("",200);
+		CreateBenchmarkResponse res2 = new CreateBenchmarkResponse(500,"");
+		res.toString();res2.toString();
+		
+		GetBenchmarkResponse ben = new GetBenchmarkResponse(null,200);		
+		GetBenchmarkResponse ben2 = new GetBenchmarkResponse(500,"");		
+		ben.toString();ben2.toString();
 		
 		Classification testClass = new Classification("test1", "desc", null);
 		classdb.addClassification(testClass);
