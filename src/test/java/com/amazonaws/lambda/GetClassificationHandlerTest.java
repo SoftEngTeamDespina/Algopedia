f0662b392d2007e8ac26210abd1dee0d2d50bb78
jolucena <64047524+jolucena@users.noreply.github.com>
2021-12-14 18:41:04 -0300

    get Classification test

--- src/test/java/com/amazonaws/lambda/GetClassificationHandlerTest.java
+++ src/test/java/com/amazonaws/lambda/GetClassificationHandlerTest.java
@@ -39,12 +39,12 @@ public class GetClassificationHandlerTest {
         return ctx;
     }
 
-    void testGetImplementationsAll(String incoming, String outgoing) throws IOException{
+    void testGetClassificationsAll(String incoming, String outgoing) throws IOException{
         try {
-            GetImplementationsAllHandler handler  = new GetImplementationsAllHandler();
+            GetClassificationHandler handler  = new GetClassificationHandler();
             InputStream input = new ByteArrayInputStream(incoming.getBytes());
             OutputStream output = new ByteArrayOutputStream();
-            handler.handleRequest(input, output, createContext("get all implementations"));
+            handler.handleRequest(input, output, createContext("get classification, alorithm, and hierarchy"));
             JsonNode outputNode = Jackson.fromJsonString(output.toString(), JsonNode.class);
             String outputString = outputNode.get("errorMessage").asText();
             if (!outputString.equals("")) {
@@ -58,33 +58,27 @@ public class GetClassificationHandlerTest {
     }
 
     @Test
-    public void testGetImplementations() throws Exception{
+    public void testGetClassification() throws Exception{
         try {
             AlgorithmDAO algodb = new AlgorithmDAO();
             ClassificationDAO classdb = new ClassificationDAO();
-            ImplementationDAO impdb = new ImplementationDAO();
 
-            Classification testClass = new Classification("testClassGI", "desc", null);
+            Classification testClass = new Classification("testClassGC", "desc", null);
             classdb.addClassification(testClass);
 
-            String testClassID = classdb.getClassification("testClassGI").getClassificationID();
+            String testClassID = classdb.getClassification("testClassGC").getClassificationID();
 
-            Algorithm algo =  new Algorithm("testAlgoGI", "desc", testClassID);
+            Algorithm algo =  new Algorithm("testAlgoGC", "desc", testClassID);
             algodb.addAlgorithm(algo);
-            String algoID = algodb.getAlgorithm("testAlgoGI").getAlgorithmID();
+            String algoID = algodb.getAlgorithm("testAlgoGC").getAlgorithmID();
             
-            String testTimeStamp = "testTimeStamp";
-            Implementation imp = new Implementation(testTimeStamp,"testLanguage","testFilename",algoID);
-            impdb.addImplementation(imp);
-            String impID = impdb.getImplementationByStamp(testTimeStamp).getImplementationID();
             
-            String input = "{\"algorithm\": \"testAlgoGI\"}";
+            String input = "{}";
             String output = "";
 
 
-            testGetImplementationsAll(input, output);
+            testGetClassificationsAll(input, output);
             
-            impdb.deleteImplementation(impID);
             algodb.removeAlgorithm(algoID);
             classdb.removeClassification(testClassID);  
 
@@ -95,23 +89,7 @@ public class GetClassificationHandlerTest {
     }
 
 
-    @Test
-    public void testBadGetImplementations() throws Exception{
-        try {
-
-            String algorithm = "Algorithm that does not exist";
-
-            String input = "{\"algorithm\": \""+algorithm+"\"}";
-            String output = "Failed";
-
-
-            testGetImplementationsAll(input, output);
-
-        } catch (Exception e) {
-            fail("Invalid"+ e);
-            System.out.println("error: " +e);
-        }
-    }
+    
     public static byte[] fromHexString(String src) {
         byte[] biBytes = new BigInteger("10" + src.replaceAll("\\s", ""), 16).toByteArray();
         return Arrays.copyOfRange(biBytes, 1, biBytes.length);
